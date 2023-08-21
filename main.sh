2182ad0ddf11f4b220afb86414cb07cda7182ee7
darius <dariuskt@gmail.com>
2023-08-21 05:26:46 +0300

    added three more items to the auto ordering list

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@
 
 cd $(dirname $(realpath $0))
 
-function addToBasketIfLowerThen() {
+function addToBasketIfLowerThenLegacy() {
 	name=$1
 	limit=$2
 	pieces=$(timeout 2 mosquitto_sub -h $mqtt_host -t scale/$name-scale/data -C 1 | jq '.pieces')
@@ -28,10 +28,33 @@ function addToBasketIfLowerThen() {
 	fi
 }
 
+function addToBasketIfLowerThen() {
+	name=$1
+	target=$2
+	pieces=$(timeout 2 mosquitto_sub -h $mqtt_host -t inventory/$name)
+	pieces="$(printf "%.0f" "$pieces")"
+
+	if [ -z $pieces ]; then
+		echo "Failed to get data for $name"
+		return
+	fi
+
+	if [ $pieces -lt $target ]; then
+		echo "$name is low($pieces), adding to the basket."
+		./barbora_add_item.sh "$name" "$((target-pieces))"
+	else
+		echo "$name is plenty($pieces) more then a target($target) no need to do anything."
+	fi
+}
+
+
 # =======================
 # list of items to automatically order with a minimum ammount to maintain
 
-addToBasketIfLowerThen shitpaper 17
-addToBasketIfLowerThen beer 12
+addToBasketIfLowerThenLegacy shitpaper 17
+addToBasketIfLowerThenLegacy beer 12
 
+addToBasketIfLowerThen napkins 4
+addToBasketIfLowerThen cotton-buds 4
+addToBasketIfLowerThen cotton-pads 3
 
