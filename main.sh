513f2bd14aec83f925d92fc1182ae73706588e31
dariuskt <dariuskt@users.noreply.github.com>
2024-10-01 19:52:11 +0300

    removing legacy inventory tracking

--- main.sh
+++ main.sh
@@ -5,28 +5,10 @@
 # email
 # password
 #
-# Dependencies: mosquitto_sub timeout jq
+# Dependencies: mosquitto_sub timeout
 #
 
 cd $(dirname $(realpath $0))
-
-function addToBasketIfLowerThenLegacy() {
-	name=$1
-	limit=$2
-	pieces=$(timeout 2 mosquitto_sub -h $mqtt_host -t scale/$name-scale/data -C 1 | jq '.pieces')
-
-	if [ -z $pieces ]; then
-		echo "Failed to get data for $name"
-		return
-	fi
-
-	if [ $pieces -lt $limit ]; then
-		echo "$name is low($pieces), adding to the basket."
-		./barbora_add_item.sh "$name"
-	else
-		echo "$name is plenty($pieces) more then a limit($limit) no need to do anything."
-	fi
-}
 
 function addToBasketIfLowerThen() {
 	name=$1
@@ -51,8 +33,8 @@ function addToBasketIfLowerThen() {
 # =======================
 # list of items to automatically order with a minimum ammount to maintain
 
-addToBasketIfLowerThenLegacy shitpaper 17
-addToBasketIfLowerThenLegacy beer 12
+addToBasketIfLowerThen shitpaper 17
+addToBasketIfLowerThen beer 12
 
 addToBasketIfLowerThen napkins 5
 addToBasketIfLowerThen cotton-buds 4
